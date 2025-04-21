5092ab8c05ea8efb826f5aba52ff763b496ea760
Parth Chavan <parthchavan653@gmail.com>
2025-04-21 17:38:59 +0530

    basic

---
+++ operators.py
@@ -0,0 +1,31 @@
+a=10 #1010
+b=4  #0100
+
+#arithmatic
+print("addition==",(a + b))
+print("substration==",(a - b))
+print("multi==",(a * b))
+print("div==",(a / b))
+print("modulas==",(a % b))
+print("exponent **==",(a ** b))
+print("floor division //==",(a // b))
+
+print("/////////////////////////////////////")
+
+#logical
+print("and==",(a < b) and (b > a))
+print("or==",(a < b) or (b < a))
+print("addition==",not(a > b))
+
+print("/////////////////////////////////////")
+
+#bitwise 
+print("&==",(a & b))
+print("|==",(a | b))
+print("~b==",(~ b))
+print("^==",(a ^ b))
+print(">>==",(a >> b))
+print("<<==",(a << b))
+
+print("/////////////////////////////////////")
+
---
+++ sqroot.py
@@ -0,0 +1,6 @@
+num= int(input("enter the number for squre root=="))
+if num>=0:
+	sqroot=num**0.5
+	print(f"sqroot of {num} is {sqroot}")
+else:
+	print(f"sqroot of {num} not exist ")
